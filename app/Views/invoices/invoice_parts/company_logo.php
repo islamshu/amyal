3db070b7f54b0a8887eacfa882148cdfbfe89529
islamshu12 <islamshu12@gmail.com>
2022-01-31 11:34:36 +0200

    s

--- app/Views/invoices/invoice_parts/company_logo.php
+++ app/Views/invoices/invoice_parts/company_logo.php
@@ -1,2 +1,2 @@
 
-<img width="300" src="<?php echo get_file_from_setting('invoice_logo', get_setting('only_file_path')); ?>" />
+<img width="300" src="<?php echo get_file_from_setting('invoice_logo', get_setting('only_file_path')); ?>" /> 
