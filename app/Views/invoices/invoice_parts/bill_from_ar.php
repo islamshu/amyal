79b3f79f5aa3bca4ca715f641ad3cdfdcef912cc
islamshu12 <islamshu12@gmail.com>
2022-01-31 10:48:01 +0200

    ر

--- app/Views/invoices/invoice_parts/bill_from_ar.php
+++ app/Views/invoices/invoice_parts/bill_from_ar.php
@@ -5,7 +5,7 @@ $company_email = get_setting("company_email");
 $company_website = get_setting("company_website");
 $company_vat_number = get_setting("company_vat_number");
 ?>
-<div><b>صاردة من </b></div>
+<div><b>صادرة من </b></div>
 <div><b><?php echo get_setting("company_name"); ?></b></div>
 <div style="line-height: 3px;"> </div>
 <span class="invoice-meta text-default" style="font-size: 90%; color: #666;"><?php
