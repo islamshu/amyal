46500ebd8882b0c4b03f207749723533d9babebe
islamshu12 <islamshu12@gmail.com>
2022-02-02 09:52:47 +0200

    X

--- app/Views/invoices/invoice_parts/bill_from.php
+++ app/Views/invoices/invoice_parts/bill_from.php
@@ -8,34 +8,35 @@ $company_vat_number = get_setting("company_vat_number");
 <div><b><?php echo app_lang("from_to"); ?></b></div>
 <div><b><?php echo get_setting("company_name"); ?></b></div>
 <div style="line-height: 3px;"> </div>
-<span class="invoice-meta text-default" style="font-size: 80%; color: #666; width: 100%; "><?php
-                                                                                            if ($company_address) {
-                                                                                                echo $company_address;
-                                                                                            }
-                                                                                            ?>
-    <? //php if ($company_phone) { 
-    ?>
-    <br /><? //php echo app_lang("phone") . ": " . $company_phone; 
-            ?>
-    <?php // } 
-    ?>
-    <?php // if ($company_email) { 
-    ?>
-    <br /><? //php echo app_lang("email") . ": " . $company_email; 
-            ?>
-    <?php // } 
-    ?>
-    <br />Riyadh
-    <br />13524
-    <br />Saudi Arabia
-    <br />VAT Number:310312014300003
-    <br />Address: Al Olaya - Riyadh
+<span class="invoice-meta text-default" style="font-size: 80%; color: #666; width: 100%; ">
+        <?php
+        if ($company_address) {
+                echo $company_address;
+        }
+        ?>
+        <? //php if ($company_phone) { 
+        ?>
+        <br /><? //php echo app_lang("phone") . ": " . $company_phone; 
+                ?>
+        <?php // } 
+        ?>
+        <?php // if ($company_email) { 
+        ?>
+        <br /><? //php echo app_lang("email") . ": " . $company_email; 
+                ?>
+        <?php // } 
+        ?>
+        <br />Riyadh
+        <br />13524
+        <br />Saudi Arabia
+        <br />VAT Number:310312014300003
+        <br />Address: Al Olaya - Riyadh
 
-    <br />
-    <?php // if ($company_vat_number) { 
-    ?>
-    <br /><?php  // echo app_lang("vat_number") . ": " . $company_vat_number; 
-            ?>
-    <?php // } 
-    ?>
+        <br />
+        <?php // if ($company_vat_number) { 
+        ?>
+        <br /><?php  // echo app_lang("vat_number") . ": " . $company_vat_number; 
+                ?>
+        <?php // } 
+        ?>
 </span>
