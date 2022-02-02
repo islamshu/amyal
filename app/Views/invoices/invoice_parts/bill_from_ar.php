f788f18efa60f22c16760794c80991cbcabff360
islamshu12 <islamshu12@gmail.com>
2022-02-02 10:39:15 +0200

    j

--- app/Views/invoices/invoice_parts/bill_from_ar.php
+++ app/Views/invoices/invoice_parts/bill_from_ar.php
@@ -9,11 +9,12 @@ $company_vat_number = get_setting("company_vat_number");
 <div><b><?php echo get_setting("company_name"); ?></b></div>
 <div style="line-height: 3px;"> </div>
 <span class="invoice-meta text-default" style="font-size: 80%; color: #666; width: 100%;">
+<br />Riyadh
+        <br>13524 
+        <br />Saudi Arabia
+        <br />VAT Number:310312014300003
+        <br />Address: Al Olaya - Riyadh
 
-    <br /> الرياض
-    <br />13524
-    <br />المملكة العربية السهودية
-    <br />ر.ض:310312014300003
-
+        <br />
 
 </span>
