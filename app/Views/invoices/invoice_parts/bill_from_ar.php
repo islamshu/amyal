a323459b7cada9c612f1e63a297ab1965d0d3289
islamshu12 <islamshu12@gmail.com>
2022-02-17 11:37:06 +0200

    ب

--- app/Views/invoices/invoice_parts/bill_from_ar.php
+++ app/Views/invoices/invoice_parts/bill_from_ar.php
@@ -9,7 +9,7 @@ $company_vat_number = get_setting("company_vat_number");
 <div><b><?php echo get_setting("company_name"); ?></b></div>
 <div style="line-height: 3px;"> </div>
 <span class="invoice-meta text-default" style="font-size: 80%; color: #666; width: 100%;">
-<br />٢٣٨٥ طريق مكة المكرمة -حي السليمانية-الرياض
+<br />٢٣٨٥ طريق مكة المكرمة -حي السليمانية الرياض
         <br>13524 
         <br />المملكة العربيه السعوديه
         <br />الرقم الضريبي:310312014300003
